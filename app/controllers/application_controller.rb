ef335da1936a1191792ee4e264eb9aeb2594a69c
iwana0187 <iwana.alpi.ishibashi@icloud.com>
2019-12-23 12:55:51 +0900

    user用のsign_in機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  def after_sign_in_path_for(resource)
+    if user_signed_in?
+      new_book_path
+    elsif store_signed_in?
+      stores_path
+    end
+  end
 end
